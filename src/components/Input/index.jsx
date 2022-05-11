cd7ae373275d88b702c693ebd20bde30d21fec75
Anas Tazir <atazir2018@gmail.com>
2022-05-11 22:39:55 +0530

    add input field for only numbers

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,11 +1,30 @@
-const Input = ({ onChange, value, placeHolder }) => (
-  <input
-    type="text"
+const Input = ({ onChange, value, placeHolder, type="text" }) => {
+
+  if (type === "text"){
+    return (
+      <input
+      type="text"
+      value={value}
+      onChange={onChange}
+      placeholder={placeHolder}
+      className="input"
+    />
+    )
+  }
+
+  return (
+    <input
+    type="number"
+    min={0}
+    max={100}
+    step={5}
     value={value}
     onChange={onChange}
     placeholder={placeHolder}
     className="input"
   />
-);
+  )
+
+}
 
 export default Input;
